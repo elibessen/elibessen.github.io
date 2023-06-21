d8976e73a0f83f4d61361d6bf0afb3506f645a7e
elibessen <ebessen@internode.on.net>
2023-06-21 15:32:51 +0930

    Made the footer even cooler!

---
+++ docs/js/dynamic-bg.js
@@ -0,0 +1,21 @@
+const [red, green, blue] = [255, 255, 255];
+const changedSection = document.querySelector('body');
+const footer = document.querySelector("#footer")
+
+let elementPosition = (footer.getBoundingClientRect().y) / 150;
+
+$(window).resize(function(){
+    elementPosition = (footer.getBoundingClientRect().y) / 150;
+})
+
+window.addEventListener('scroll', () => {
+    let y = 1 + (window.scrollY) / 150;
+    if (y < elementPosition + 5){
+        y = 1;
+    }else{
+        y = y - elementPosition - 5;
+        const [r, g, b] = [red/y-elementPosition - 45, green/y-elementPosition - 45, blue/y-elementPosition- 45].map(Math.round);
+        changedSection.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        document.querySelector(".footer").style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
+})

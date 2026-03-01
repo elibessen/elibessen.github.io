b254115586840e1e32c719b57b94a497f4c3ba2f
elibessen <eli.bessen04@gmail.com>
2026-03-01 12:48:01 +1100

    updated: details and added last update to repos

--- src/compile.js
+++ src/compile.js
@@ -2,13 +2,14 @@ const pug = require("pug");
 const fs = require("fs");
 
 // List of Pug files to compile
-const files = ["src/index", "src/404"];
+const files = ["index", "404"];
+
 
 files.forEach((file) => {
   // Compile Pug to HTML string
-  const html = pug.renderFile(`${file}.pug`, { pretty: true });
+  const html = pug.renderFile(`src/${file}.pug`, { pretty: true });
 
   // Write HTML to corresponding .html file
-  fs.writeFileSync(`${file}.html`, html, "utf8");
+  fs.writeFileSync(`docs/${file}.html`, html, "utf8");
   console.log(`${file}.pug compiled to ${file}.html`);
 });
